92cd212a2ab8ce733125ea60d416773f0cef4294
Lauren Johnston <hilotraining@outlook.com>
2023-06-08 13:17:34 -0600

    npm init with express server set up

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+const port = 8000;
+
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/', (req, res) => {
+  res.send('hello world from express');
+});
+
+//import router
+
+
+//use router
+
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+});
