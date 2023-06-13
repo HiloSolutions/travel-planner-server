06c416411616868ba25a7114a0d659951e104025
Lauren Johnston <hilotraining@outlook.com>
2023-06-13 12:17:50 -0600

    added insert statement for users upon registration and login

--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,75 @@
 const express = require("express");
 const router = express.Router();
-const axios = require('axios');
 const db = require("../db/connection");
 
-// router.get('/register', (req, res) => {
-  
 
-//   const queryParams = {
-//   };
+//Check if user exists
+router.get('/check', (req, res) => {
+  console.log("getting user data!");
 
-//   axios.get(url, queryParams)
-//     .then(response => {
-     
-//       res.json();
-//     })
-//     .catch(error => {
-//       console.error('Registration Error:', error);
-//     });
-  
+  const queryStr = `
+  SELECT * FROM users
+  WHERE users.id = 1
+  `;
 
-// });
+
+  db.query(queryStr)
+    .then((result) => {
+      console.log('jhsgdhsg', result.rows);
+      res.json(result.rows);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+
+});
+
+
+//add user to database if they don't already exist.
+router.post('/update', (req, res) => {
+  const sub = req.query.sub;
+
+  //check if user exists
+  const selectQuery = `
+  SELECT * FROM users
+  WHERE sub = $1;
+`;
+  const selectParams = [sub];
+
+  db.query(selectQuery, selectParams)
+    .then((result) => {
+
+      if (result.rows.length > 0) {
+
+        // sub value already exists in the database
+        console.log('sub value already exists');
+        res.json({ message: 'sub value already exists' });
+
+      } else {
+
+        // sub value does not exist, perform insert query
+        const insertQuery = `
+          INSERT INTO users (sub)
+          VALUES ($1);
+        `;
+        const insertParams = [sub];
+
+        db.query(insertQuery, insertParams)
+          .then(() => {
+            console.log('added user to db');
+            res.json({ message: 'User added to database' });
+          })
+          .catch((err) => {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred' });
+          });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: 'An error occurred' });
+    });
+});
 
 module.exports = router;
